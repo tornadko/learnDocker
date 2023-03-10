37f1cc138a78b1c297857d6791bd1ea3f33f3e74
Alex Kucherenko <a.kucherenko@viseven.com>
2023-03-10 13:45:55 +0200

    Docker compose. MongoDB

--- realworld-docker/api/src/index.js
+++ realworld-docker/api/src/index.js
@@ -1,14 +1,54 @@
 'use strict';
-
+const config = require('./configuration');
 const express = require("express")
+const connectDB = require('./utils/db');
+const User = require('./models/user');
+
 const app = express()
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = config.port;
+const HOST = config.host;
+
+app.use(express.json());
 
 app.get("/test", (req, res) => {
 	res.send("Server is working correctly")
 })
 
-app.listen(PORT, HOST, () => {
-	console.log("API-service is working")
-})
+// Connect to database
+connectDB()
+  .then(() => {
+    // Start server
+    app.listen(PORT, HOST, () => {
+		  console.log(`API-service is working on port ${PORT}`)
+	  })
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Add a user
+// To add call: curl -X POST -H "Content-Type: application/json" -d '{"userName": "Alex K"}' http://localhost:80/users
+app.post('/users', (req, res) => {
+  const { userName } = req.body;
+
+  const user = new User({ userName });
+
+  user.save().then((user) => {
+     res.send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal server error');
+    });
+});
+
+// Get users
+app.get('/users', (req, res) => {
+  User.find().exec().then(users => {
+    res.send(users);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).send('Internal server error');
+  });
+});
